8ddf994e43f3452567a05f310957931a1101b38c
Amulya <amulyayarasani@gmail.com>
2023-03-23 12:43:14 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,11 @@
+REM   Script: Activity1.sql
+REM   Activity1
+
+CREATE TABLE SALESMAN( 
+  SALESMAN_ID int not null, 
+  SALESMAN_NAME Varchar2(32), 
+  SALESMAN_CITY varchar2(32), 
+  COMMISSION int 
+  );
+
+  Describe salesman
